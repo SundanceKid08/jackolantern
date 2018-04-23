db58a3a2376b5e5afab398c296f4e8d3532e4077
Darkkenfloxx <mblanch08@gmail.com>
2018-04-23 14:43:05 -0400

    Adding starting contents
    Add contents from original project starting point

---
+++ src/GameLevel.lua
@@ -0,0 +1,17 @@
+
+
+GameLevel = Class{}
+
+function GameLevel:init(level)
+    self.tiles = level.tiles
+    self.entities = level.entities
+    self.objects = level.objects
+end
+
+function GameLevel:update(dt)
+
+end
+
+function GameLevel:render()
+
+end
---
+++ src/constants.lua
@@ -0,0 +1,14 @@
+--[[
+
+    Escape Game constants
+]]
+
+
+
+--actual window size
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 246
+VIRTUAL_HEIGHT = 144
